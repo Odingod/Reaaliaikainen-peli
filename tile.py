4124926da12bbd956c849e809464cd7ce81fbd19
hanzki <hannu.huhtanen@gmail.com>
2012-03-13 02:10:43 +0200

    Update tile.py

--- tile.py
+++ tile.py
@@ -5,7 +5,12 @@ TILESIZE = 16
 
 class Tile(Sprite):
 
-    # flags = 0
+    F_VISIBLE = 0
+    F_SOLID = 1
+    F_FIRE = 2
+    F_AWESOME = 3
     
-    def __init__(self):
+    def __init__(self, *flags):
         Sprite.__init__(self)
+        self.flags = list(flags)
+        
