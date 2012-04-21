3b17baf85768a367316eaa6032503a3a26ab43b2
Eetu Haavisto <eetu.haavisto@aalto.fi>
2012-04-21 22:35:23 +0300

    Added character animation

--- player.py
+++ player.py
@@ -13,11 +13,18 @@ class Player(Character):
         eventMgr.register(self)
         
         self.keys = [False] * 5
-        self.image = pygame.image.load("media/Character.png")
+        self.animation_frames = map(pygame.image.load, ["media/nja2_lf1.png", "media/nja2_lf2.png", "media/nja2_rt1.png", "media/nja2_rt2.png", "media/nja2_fr1.png"])
+        self.frame_count = 2
+        self.image = self.animation_frames[0]
+        self.animation_step = 0
+        self.frame_interval = 10
+        self.frame = 0
+        self.going_left = False
+        self.going_right = False
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
-        self.rect.width = 47
-        self.rect.height = 64
+        self.rect.width = 32
+        self.rect.height = 32
         self.jumped = False
         self.createBody(b2World, self.rect.width, self.rect.height )
         
@@ -38,11 +45,25 @@ class Player(Character):
             self.body.linearVelocity = (400 * B2SCALE, self.body.linearVelocity[1] )
         if self.keys[1]:
             if abs(self.body.linearVelocity[1] ) < 0.01:
-                self.body.ApplyLinearImpulse(impulse=(0, -480 * B2SCALE), point=(0,0))
+                self.body.ApplyLinearImpulse(impulse=(0, -200 * B2SCALE), point=(0,0))
                 self.jumped = True
         else:
             self.jumped = False
-            
+        
+        self.going_left = self.body.linearVelocity[0] < -0.1
+        self.going_right = self.body.linearVelocity[0] > 0.1
+        self.update_animation(dt)
+
+    def update_animation(self, dt):
+        self.animation_step += dt
+        self.frame = (self.animation_step / self.frame_interval) % self.frame_count
+        if self.going_left:
+            self.image = self.animation_frames[self.frame]
+        elif self.going_right:
+            self.image = self.animation_frames[self.frame + self.frame_count]
+        else:
+            self.image = self.animation_frames[-1]
+
     def notify(self, event):
         if event.name == 'Keyboard':
             key = event.key
